05d46838a496a65c42ecfd773e3940f9f5e57e34
hashimati <hashimati.ahmed@gmail.com>
2013-09-11 09:57:08 +0300

    upload Search Agent

--- src/com/ahmed/main/SearchAgent.java
+++ src/com/ahmed/main/SearchAgent.java
@@ -1,5 +1,50 @@
 package com.ahmed.main;
 
-public class SearchAgent {
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.io.*;
+
+public class SearchAgent implements Runnable {
+	String pattern; 
+	ConcurrentLinkedQueue<File> queue;
+	ConcurrentLinkedQueue<String> searchResult;
+	public boolean isRoot(File f){
+		File roots[] = File.listRoots(); 
+		for(File i : roots){
+			if(f.getAbsolutePath().equals(i.getAbsolutePath()))
+				return true; 
+		}
+		return false; 
+	}
+	public SearchAgent(ConcurrentLinkedQueue<File> queue,
+			ConcurrentLinkedQueue<String> searchResult, String pattern) {
+
+		this.queue = queue;
+		this.searchResult = searchResult;
+		this.pattern = pattern; 
+	}
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		try {
+
+			while (queue.isEmpty() == false) {
+				File current = queue.remove();
+				if (current.getAbsolutePath().toLowerCase().contains(pattern.toLowerCase()))
+					searchResult.add(current.getAbsolutePath());
+				if (current.isDirectory() || isRoot(current)) {
+						File childern[] = current.listFiles();
+						if(childern != null)
+							for (File c : childern) 
+									queue.add(c);
+				}
+				
+				Thread.sleep(100); 
+			}
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
 
 }
